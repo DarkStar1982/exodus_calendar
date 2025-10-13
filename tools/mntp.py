151a67f8d6f502635f5800f9fd94c4d9333a9dce
Dennis Silin <darkft@gmail.com>
2025-10-13 12:59:48 -0400

    Added more MNTP demo code

--- tools/mntp.py
+++ tools/mntp.py
@@ -18,6 +18,29 @@ import mntp_pb2_grpc
 DEFAULT_PORT = 1955
 DEFAULT_SERVER = "localhost"
 
+
+def check_packet_integrity(packet):
+	decoded = {
+		"header":packet.Header,
+		"root_delay": packet.RootDelay,
+		"dispersion": packet.Dispersion,
+		"reference_id": packet.ReferenceID,
+		"reference_timestamp": packet.ReferenceTimestamp,
+		"origin_timestamp": packet.OriginTimestamp,
+		"receive_timestamp": packet.ReceiveTimestamp,
+		"transmit_timestamp": packet.TransmitTimestamp
+	}
+
+	# encode packet contents
+	packet_as_str = ''
+	for value in decoded.values():
+		packet_as_str = packet_as_str + str(value)
+	
+	# add hashsum
+	str_bytes= packet_as_str.encode('utf-8')
+	sha256hex = hashlib.sha256(str_bytes).hexdigest()
+	return (sha256hex == packet.SHA2Checksum)
+
 class MNTP_Client():
 	version = 1
 	mode = 0
@@ -34,9 +57,6 @@ class MNTP_Client():
 			"root_delay": 0,
 			"dispersion": 0,
 			'reference_id': 0,
-			'root_delay': 0,
-			'dispersion': 0,
-			'reference_id':0,
 			'reference_timestamp':0,
 			'origin_timestamp':0,
 			'receive_timestamp':0,
@@ -68,13 +88,16 @@ class MNTP_Client():
 				TransmitTimestamp = client_packet["transmit_timestamp"], 
 				SHA2Checksum = client_packet['sha2checksum']
 			))
-			T4 = mars_datetime_now(format="ms")
-			T3 = response.TransmitTimestamp
-			T2 = response.ReceiveTimestamp
-			T1 = response.OriginTimestamp
-			print(f"T1:{T1}, T2:{T2}, T3:{T3}, T4:{T4}")
-			print(f"d={(T4 - T1) - (T3 - T2)}")
-			print(f"t={((T2 - T1) + (T3 - T4))/2}")
+			if check_packet_integrity(response):
+				T4 = mars_datetime_now(format="ms")
+				T3 = response.TransmitTimestamp
+				T2 = response.ReceiveTimestamp
+				T1 = response.OriginTimestamp
+				print(f"T1:{T1}, T2:{T2}, T3:{T3}, T4:{T4}")
+				print(f"d={(T4 - T1) - (T3 - T2)}")
+				print(f"t={((T2 - T1) + (T3 - T4))/2}")
+			else:
+				raise Exception("Server packet checksum failed!")
 
 
 class MNTP_Server(mntp_pb2_grpc.MNTP_Service):
@@ -83,58 +106,47 @@ class MNTP_Server(mntp_pb2_grpc.MNTP_Service):
 	poll = 1
 	precision = 1
 	ref_timestamp = mars_datetime_now(format="ms")
-	
-	def check_packet_integrity(self, packet):
-		packet_as_str = ''
-
-		decoded = {
-			"header":packet.Header,
-			"root_delay": packet.RootDelay,
-			"dispersion": packet.Dispersion,
-			"reference_id": packet.ReferenceID,
-			"reference_timestamp": packet.ReferenceTimestamp,
-			"receive_timestamp": packet.ReceiveTimestamp,
-			"origin_timestamp": packet.OriginTimestamp,
-			"transmit_timestamp": packet.TransmitTimestamp
-		}
-
-		packet_as_str = ''
-		for value in decoded.values():
-			packet_as_str = packet_as_str + str(value)
-		
-		str_bytes= packet_as_str.encode('utf-8')
-		sha256hex = hashlib.sha256(str_bytes).hexdigest()
-		assert(sha256hex ==packet.SHA2Checksum)
-		return True
 
 	def decode_client_packet(self, packet):
-		integrity = self.check_packet_integrity(packet)
-		client_version = packet.Header>>24 & 0x000000FF
-		mode = packet.Header>>16 & 0x000000FF
-		poll = packet.Header>>8 & 0x000000FF
-		precision = packet.Header & 0x000000FF
-		sha2hash = packet.SHA2Checksum
-		print(f"Client packet header: {client_version} | {mode} | {poll} | {precision}")
-		print(f"Client packet checksum: {sha2hash}")
-		return (packet.TransmitTimestamp, packet.Header)
+		if check_packet_integrity(packet):
+			client_version = packet.Header>>24 & 0x000000FF
+			mode = packet.Header>>16 & 0x000000FF
+			poll = packet.Header>>8 & 0x000000FF
+			precision = packet.Header & 0x000000FF
+			sha2hash = packet.SHA2Checksum
+			print(f"Client packet header: {client_version} | {mode} | {poll} | {precision}")
+			print(f"Client packet checksum: {sha2hash}")
+			return (packet.TransmitTimestamp, packet.Header)
+		else:
+			raise Exception("Packet checksum failed")
 
-	def form_server_packet(self):
+	def form_server_packet(self, origin_ts, receive_ts):
 		packet = {
 			"header": self.version_info<<24 | self.mode<<16 | self.poll<<8 | self.precision,
 			"root_delay": 0,
 			"dispersion": 0,
 			"reference_id":1,
 			"reference_timestamp": self.ref_timestamp,
-			"origin_timestamp": 0,
-			"receive_timestamp": mars_datetime_now(format="ms"),
-			"transmit_timestamp":0,
-			"sha2checksum":'0'
+			"origin_timestamp": origin_ts,
+			"receive_timestamp": receive_ts,
+			"transmit_timestamp":mars_datetime_now(format="ms"),
 		}
+
+		# encode packet contents
+		packet_as_str = ''
+		for value in packet.values():
+			packet_as_str = packet_as_str + str(value)
+		# add hashsum		
+		str_bytes= packet_as_str.encode('utf-8')
+		sha256hex = hashlib.sha256(str_bytes).hexdigest()
+		packet['sha2checksum'] = sha256hex
 		return packet
 
 	def ProcessTimeSyncRequest(self, request, context):
-		packet = self.form_server_packet()
-		result, header = self.decode_client_packet(request)
+		receive_ts = mars_datetime_now(format="ms")
+		origin_ts, header = self.decode_client_packet(request)
+		packet = self.form_server_packet(origin_ts, receive_ts)
+
 		print("Client packet received: " + str(header))
 		return mntp_pb2.MNTP_Packet(
 			Header = packet["header"], 
@@ -142,10 +154,10 @@ class MNTP_Server(mntp_pb2_grpc.MNTP_Service):
 			Dispersion = packet["dispersion"], 
 			ReferenceID = packet["reference_id"],
             ReferenceTimestamp = packet["reference_timestamp"], 
-            OriginTimestamp = result, 
+            OriginTimestamp = packet["origin_timestamp"], 
             ReceiveTimestamp = packet["receive_timestamp"],
-            TransmitTimestamp = mars_datetime_now(format="ms"), 
-            SHA2Checksum = 'B'
+            TransmitTimestamp = packet["transmit_timestamp"],
+            SHA2Checksum = packet['sha2checksum']
         )
 
 
